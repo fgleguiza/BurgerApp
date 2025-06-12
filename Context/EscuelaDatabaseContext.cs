177ce9b5de3cbd6a6627b6ea7507e3dedd73f914
fgleguiza <fgleguizadeveloper@gmail.com>
2025-06-11 22:51:08 -0300

    primer comit + push

---
+++ Context/EscuelaDatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using burgerApp.Models;
+using System.Collections.Generic;
+
+namespace burgerApp.Context
+{
+    public class EscuelaDatabaseContext : DbContext
+    {
+        public EscuelaDatabaseContext(DbContextOptions<EscuelaDatabaseContext> options) : base(options)
+        {
+        }
+        public DbSet<Estudiante> Estudiantes { get; set; }
+    }
+
+}
---
+++ Models/Deporte.cs
@@ -0,0 +1,13 @@
+﻿namespace burgerApp.Models
+{
+    public enum Deporte
+    {
+        Natacion,
+        Futbol,
+        Voley,
+        Basquet,
+        Truco,
+        Ajedrez
+    }
+
+}
